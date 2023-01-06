e249b806e1ee7308ddbd5e899dd8836bdb7d4a91
sorin.janosi <sorin.janosi@gmail.com>
2023-01-06 12:24:20 +0100

    Update download_candles.py

--- download_candles.py
+++ download_candles.py
@@ -1,20 +1,19 @@
 from datetime import date, timedelta
 import os
-import urllib.request
+import requests
 import zipfile
+import io
+
 
 # the symbol pairs to download candles for
 symbols = ['ETHBUSD']
-
 # the timeframes to download candles for
 timeframes = ['1m', '5m', '15m', '1h']
-
 # binance URL
-binance_url = "https://data.binance.vision/?prefix=data/spot/daily/klines"
-
+binance_url = "https://data.binance.vision/data/spot/daily/klines"
 # the home directory
+# home_dir = "/Users/sorinjanosi/klines"
 home_dir = "/home/ubuntu/klines"
-
 # get the current day
 today = date.today()
 yesterday = today - timedelta(1)
@@ -36,19 +35,20 @@ for symbol in symbols:
 
 		# download all daily files
 		current_day = first_day
-		while current_day != yesterday:
+		while current_day != today:
 			# the file to download
 			filename = current_day.strftime(f"{symbol}-{timeframe}-%Y-%m-%d")
 			# check if file exist already
 			if not os.path.exists(f"{timeframe_dir}/{filename}.csv"):
 				# download the file
-				url = f"{binance_url}/{symbol}/{timeframe}/{filename}.zip"
+				zip_file_url = f"{binance_url}/{symbol}/{timeframe}/{filename}.zip"
 				local_zipfile = f"{timeframe_dir}/{filename}.zip"
-				urllib.request.urlretrieve(url, local_zipfile)
-				# extract the zip archive
-				with zipfile.ZipFile(local_zipfile, 'r') as zip_ref:
-					zip_ref.extractall(timeframe_dir)
+				# download the zip file
+				r = requests.get(zip_file_url, stream=True)
+				z = zipfile.ZipFile(io.BytesIO(r.content))
+				z.extractall(timeframe_dir)
+				z.close()
 				# delete the zip archive
-				os.remove(local_zipfile)
+				# os.remove(local_zipfile)
 			# increment the current day
 			current_day += timedelta(1)
